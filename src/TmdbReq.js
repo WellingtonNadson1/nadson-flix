5f6c4ad4e30a67c06dc7c0628f932895b9f964c0
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-08-01 17:48:05 -0300

    Consuming API at Tmdb

---
+++ src/TmdbReq.js
@@ -0,0 +1,54 @@
+console.log(import.meta.env.VITE_API_KEY)
+
+const basicFetch = async (endpoint) => {
+    const request = await fetch(`${import.meta.env.VITE_BASE_URL}${endpoint}${import.meta.env.VITE_API_KEY}`)
+    const json = await request.json()
+    return json
+}
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: 'Originais Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=`)
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para Você',
+                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=`)
+            },
+            {
+                slug: 'toprated',
+                title: 'Em alta',
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=`)
+            },
+            {
+                slug: 'action',
+                title: 'Ação',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=`)
+            },
+            {
+                slug: 'comedy',
+                title: 'Comédia',
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=`)
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=`)
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=`)
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=`)
+            },
+        ]
+    }
+}
